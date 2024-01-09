d030b314d2dbc5dc04b296240c1712270d902ec6
medchek <wizzsha@gmail.com>
2024-01-09 17:51:56 +0100

    feat: add startup splash screen

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -2,14 +2,28 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
+use tauri::{Manager, Window};
+
+// This command must be async so that it doesn't run on the main thread.
 #[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
+async fn close_splashscreen(window: Window) {
+    // Close splashscreen
+    window
+        .get_window("splashscreen")
+        .expect("no window labeled 'splashscreen' found")
+        .close()
+        .unwrap();
+    // Show main window
+    window
+        .get_window("main")
+        .expect("no window labeled 'main' found")
+        .show()
+        .unwrap();
 }
 
 fn main() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![close_splashscreen])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,20 @@
-import { Toaster } from "sonner";
 import { useRef, useState } from "react";
-import { useCopyToClipboard, useTimeout } from "usehooks-ts";
+import { invoke } from "@tauri-apps/api/tauri";
+import { Toaster } from "sonner";
+import { useCopyToClipboard, useEffectOnce, useTimeout } from "usehooks-ts";
 import Input from "./components/Input";
 import Output from "./components/Output";
 import Header from "./components/Header";
 import CopyButton from "./components/CopyButton";
 import Footer from "./components/Footer";
-
 export default function App() {
+  useEffectOnce(() => {
+    const splashTimeout = setTimeout(() => {
+      invoke("close_splashscreen");
+    }, 500);
+    return () => clearTimeout(splashTimeout);
+  });
+
   const outputRef = useRef<HTMLTextAreaElement | null>(null);
 
   // a null value disables the timeout
@@ -32,7 +39,7 @@ export default function App() {
   return (
     <div className="flex flex-col bg-neutral-50 dark:bg-[#06060A] dark:text-neutral-100 h-full w-full px-4 sm:px-10 md:px-24 xl:px-48 text-neutral-900 dark:[color-scheme:dark]">
       <Header />
-      <main className="flex flex-col justify-between py-4 gap-4 grow ">
+      <main className="flex flex-col justify-between pt-4 pb-2 md:pb-4 gap-4 grow ">
         <div className="flex flex-col gap-4 grow">
           <Input />
           <Output ref={outputRef} />
